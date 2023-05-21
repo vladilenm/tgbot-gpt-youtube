ee45f513a964b00c80b64fb3b9e93c104d1821de
Vladilen <mininvlad@gmail.com>
2023-05-21 13:08:31 +0300

    add readme

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Telegram бот работающий с ChatGPT
+
+## Видео инструкция к репозиторию
+
+https://youtu.be/-6ufFPvp6CY
--- src/logic.js
+++ src/logic.js
@@ -5,7 +5,7 @@ export const INITIAL_SESSION = {
 }
 
 export async function initCommand(ctx) {
-  ctx.session = INITIAL_SESSION
+  ctx.session = { ...INITIAL_SESSION }
   await ctx.reply('Жду вашего голосового или текстового сообщения')
 }
 
